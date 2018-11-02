5b1abf87960c77fe3aa7a56cf720c2e52b488e7b
Radoslaw Jaros <jaros_radek@op.pl>
2018-11-02 14:57:48 -0700

    Update changelog for 2.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jaros_radek@op.pl'
 license          'Apache-2.0'
 description      'Installs/Configures expect package'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.1.0'
+version          '2.1.1'
 source_url       'https://github.com/rjaros87/chef-expect'
 issues_url       'https://github.com/rjaros87/chef-expect/issues'
 
